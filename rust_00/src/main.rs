c4693bf89a46a05db8d1e58c1a3e3cf98ce9e783
unknown <daniel.orduy_rey@edu.devinci.fr>
2025-11-25 16:56:16 +0100

    mise a jour exo

--- rust_00/src/main.rs
+++ rust_00/src/main.rs
@@ -1,5 +1,30 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    // Le nom (par défaut "World")
+    #[arg(default_value = "World")]
+    name: String,
+
+    // Le flag majuscule
+    #[arg(long)]
+    upper: bool,
+
+    // Le nombre de répétitions (par défaut 1)
+    #[arg(short, long, default_value_t = 1)]
+    repeat: u8,
+}
+
 fn main() {
-    println!("Hello, world! zebi");
-    println!("Hello, 2! zebi");
-    println!("Hello, 3! zebi");
+    let args = Args::parse();
+
+    let mut message = format!("Hello, {}!", args.name);
+
+    if args.upper {
+        message = message.to_uppercase();
+    }
+
+    for _ in 0..args.repeat {
+        println!("{}", message);
+    }
 }
